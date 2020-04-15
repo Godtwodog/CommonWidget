f1c79db8aaa489a225350c538bbee878662595cc
邓晓升 <dengxiaosheng@yeah.net>
2020-04-15 18:12:41 +0800

    中间弹窗

--- dialoglibrary/src/main/java/com/god2dog/dialoglibrary/CenterDialog.java
+++ dialoglibrary/src/main/java/com/god2dog/dialoglibrary/CenterDialog.java
@@ -3,7 +3,9 @@ package com.god2dog.dialoglibrary;
 import android.app.Dialog;
 import android.content.Context;
 import android.view.Display;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
@@ -15,9 +17,26 @@ import androidx.annotation.NonNull;
  */
 public class CenterDialog extends Dialog {
     private Display display;
-    public CenterDialog(@NonNull Context context) {
-        super(context,R.style.Center_Dialog);
+    private int dialogStyle = 1;
 
+    private TextView mCancelAction;
+    private TextView mConfirmAction;
+    //标题
+    private TextView mTitle;
+    //副标题
+    private TextView mSubhead;
+    //文本内容
+    private TextView mContent;
+
+    private OnDialogClickedListener onDialogListener;
+
+    public void AddOnDialogListener(OnDialogClickedListener onDialogListener) {
+        this.onDialogListener = onDialogListener;
+    }
+
+    public CenterDialog(@NonNull Context context , int style) {
+        super(context,R.style.Center_Dialog);
+        this.dialogStyle = style;
         WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         if (windowManager != null){
             display = windowManager.getDefaultDisplay();
@@ -27,9 +46,27 @@ public class CenterDialog extends Dialog {
     }
 
     private void initDialog() {
-        setContentView(R.layout.layout_center_dialog);
         setCancelable(true);
         setCanceledOnTouchOutside(true);
+        if (dialogStyle == 1) {
+            setContentView(R.layout.layout_center_dialog);
+            mCancelAction = findViewById(R.id.tvCancelAction);
+            mConfirmAction = findViewById(R.id.tvConfirmAction);
+            mTitle = findViewById(R.id.tvMainTitle);
+            mSubhead = findViewById(R.id.tvSubHead);
+            mContent = findViewById(R.id.tvContent);
+        }
 
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    public interface OnDialogClickedListener{
+        void onCancelAction(View view);
+        void onConfirmAction(View view);
     }
 }
