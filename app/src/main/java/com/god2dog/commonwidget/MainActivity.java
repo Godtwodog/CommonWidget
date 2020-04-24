d05f36155be8aac48a4aad426b9615e8d2ea7c73
邓晓升 <dengxiaosheng@yeah.net>
2020-04-24 21:49:22 +0800

    添加三方 滚轮控件

--- app/src/main/java/com/god2dog/commonwidget/MainActivity.java
+++ app/src/main/java/com/god2dog/commonwidget/MainActivity.java
@@ -12,6 +12,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.bigkoo.pickerview.builder.TimePickerBuilder;
+import com.bigkoo.pickerview.listener.OnTimeSelectListener;
+import com.bigkoo.pickerview.view.TimePickerView;
 import com.god2dog.addresspickerview.AddressPickerView;
 import com.god2dog.basecode.AddressModel;
 import com.god2dog.dialoglibrary.BottomDialog;
@@ -21,10 +24,9 @@ import com.god2dog.dialoglibrary.Item;
 import com.god2dog.dialoglibrary.OnItemClickListener;
 import com.god2dog.wheelwidget.AddressSelectedView;
 import com.god2dog.wheelwidget.builder.AddressSelectedBuilder;
+import com.god2dog.wheelwidget.builder.TimeSelectBuilder;
 import com.god2dog.wheelwidget.listener.OnOptionsSelectedListener;
 import com.god2dog.wheelwidget.listener.OnTimeSelectChangeListener;
-import com.god2dog.wheelwidget.listener.OnTimeSelectListener;
-import com.god2dog.wheelwidget.builder.TimeSelectBuilder;
 import com.god2dog.wheelwidget.view.TimeSelectedView;
 
 import org.json.JSONArray;
@@ -35,11 +37,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private TimeSelectedView timer;
+    //时间选择器
+    private TimePickerView pvTime;
     private int[] i;
     private List<AddressModel> datas = new ArrayList<>();
 
@@ -62,14 +67,7 @@ public class MainActivity extends AppCompatActivity {
 
         initBottomListDialog();
 
-        Button button = findViewById(R.id.btnShowDialog);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                timer.show();
-            }
-        });
+        initTimeSelect();
 
         initJump();
 
@@ -78,40 +76,49 @@ public class MainActivity extends AppCompatActivity {
         initAddressStyleTwo();
     }
 
+    private void initTimeSelect() {
+        findViewById(R.id.showTimePickerView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pvTime.show();
+            }
+        });
+    }
+
     private void initAddressStyleTwo() {
         Button addressStyleTwoButton =findViewById(R.id.showAddressSelectedView);
 
-        formatAddressData( datas);
-
-        final AddressSelectedView aSelectedView = new AddressSelectedBuilder(this, new OnOptionsSelectedListener() {
-            @Override
-            public void onSelectedItem(int options1, int options2, int options3) {
-                //返回的分别是三个级别的选中位置
-                String opt1tx = datas.size() > 0 ?
-                        datas.get(options1).getName() : "";
-
-                String opt2tx = options2Items.size() > 0
-                        && options2Items.get(options1).size() > 0 ?
-                        options2Items.get(options1).get(options2) : "";
-
-                String opt3tx = options2Items.size() > 0
-                        && options3Items.get(options1).size() > 0
-                        && options3Items.get(options1).get(options2).size() > 0 ?
-                        options3Items.get(options1).get(options2).get(options3) : "";
-
-                String tx = opt1tx + opt2tx + opt3tx;
-                Toast.makeText(MainActivity.this, tx, Toast.LENGTH_SHORT).show();
-            }
-        })
-                .setTitleText("地址选择")
-                .setDividerColor(Color.BLACK)
-                .build();
-        aSelectedView.setData(datas,options2Items,options3Items);
+//        formatAddressData( datas);
+//
+//        final AddressSelectedView aSelectedView = new AddressSelectedBuilder(this, new OnOptionsSelectedListener() {
+//            @Override
+//            public void onSelectedItem(int options1, int options2, int options3) {
+//                //返回的分别是三个级别的选中位置
+//                String opt1tx = datas.size() > 0 ?
+//                        datas.get(options1).getName() : "";
+//
+//                String opt2tx = options2Items.size() > 0
+//                        && options2Items.get(options1).size() > 0 ?
+//                        options2Items.get(options1).get(options2) : "";
+//
+//                String opt3tx = options2Items.size() > 0
+//                        && options3Items.get(options1).size() > 0
+//                        && options3Items.get(options1).get(options2).size() > 0 ?
+//                        options3Items.get(options1).get(options2).get(options3) : "";
+//
+//                String tx = opt1tx + opt2tx + opt3tx;
+//                Toast.makeText(MainActivity.this, tx, Toast.LENGTH_SHORT).show();
+//            }
+//        })
+//                .setTitleText("地址选择")
+//                .setDividerColor(Color.BLACK)
+//                .build();
+//        aSelectedView.setData(datas,options2Items,options3Items);
 
         addressStyleTwoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                aSelectedView.show();
+               startActivity(new Intent(MainActivity.this,JsonDataActivity.class));
             }
         });
 
@@ -382,7 +389,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void initTimePicker() {//Dialog 模式下，在底部弹出
-        timer = new TimeSelectBuilder(this, new OnTimeSelectListener() {
+        timer = new TimeSelectBuilder(this, new com.god2dog.wheelwidget.listener.OnTimeSelectListener() {
 
             @Override
             public void onTimeSelected(Date date, View view) {
@@ -411,6 +418,54 @@ public class MainActivity extends AppCompatActivity {
                 .setContentTextSize(20)
                 .isCyclic(true)
                 .build();
+
+        //
+        Calendar selectedDate = Calendar.getInstance();
+        Calendar startDate = Calendar.getInstance();
+        //startDate.set(2013,1,1);
+        Calendar endDate = Calendar.getInstance();
+        //endDate.set(2020,1,1);
+
+        //正确设置方式 原因：注意事项有说明
+        startDate.set(2013,0,1);
+        endDate.set(2020,11,31);
+
+        pvTime = new TimePickerBuilder(this, new OnTimeSelectListener() {
+            @Override
+            public void onTimeSelect(Date date,View v) {//选中事件回调
+                Toast.makeText(MainActivity.this, date.toString(), Toast.LENGTH_SHORT).show();
+            }
+        })
+                .setType(new boolean[]{true, true, true, true, true, true})// 默认全部显示
+                .setCancelText("取消")//取消按钮文字
+                .setSubmitText("确认")//确认按钮文字
+                .setContentTextSize(18)//滚轮文字大小
+                .setTitleSize(20)//标题文字大小
+                .setTitleText("我是标题")//标题文字
+                .setOutSideCancelable(false)//点击屏幕，点在控件外部范围时，是否取消显示
+                .isCyclic(true)//是否循环滚动
+                .setTitleColor(Color.BLACK)//标题文字颜色
+                .setSubmitColor(Color.BLACK)//确定按钮文字颜色
+                .setCancelColor(Color.BLACK)//取消按钮文字颜色
+                .setTitleBgColor(Color.WHITE)//标题背景颜色 Night mode
+                .setBgColor(Color.WHITE)//滚轮背景颜色 Night mode
+                .setDate(selectedDate)// 如果不设置的话，默认是系统时间*/
+                .setRangDate(startDate,endDate)//起始终止年月日设定
+                .setLabel("年","月","日","时","分","秒")//默认设置为年月日时分秒
+                .isCenterLabel(false) //是否只显示中间选中项的label文字，false则每项item全部都带有label。
+                .isDialog(true)//是否显示为对话框样式
+                .build();
+
+        Button button = findViewById(R.id.btnShowDialog);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.show();
+            }
+        });
+
+
 
     }
 
